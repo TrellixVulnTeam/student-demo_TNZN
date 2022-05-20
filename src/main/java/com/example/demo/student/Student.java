7210c774cde9dbf10aa0fdcdc4c2e0137748b014
Omar Shabaan <omar.plus.077@gmail.com>
2022-05-20 13:04:23 +0200

    add StudentController to serve requests under /api/v1/students

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,10 @@
 package com.example.demo.student;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Student {
   private Long id;
   private String name;
