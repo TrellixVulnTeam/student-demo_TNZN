895ac1fd53a829a9e674a4d01658581ac26faa92
Omar Shabaan <omar.plus.077@gmail.com>
2022-05-21 03:57:05 +0200

    retrieve students data from backend and save it in a state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 
 import {getAllStudents} from "./client";
+import {useEffect, useState} from "react";
 
 function App() {
-    getAllStudents()
-        .then(res => res.json())
-        .then(console.log);
-    return "hello react";
+    const [students, setStudents] = useState([]);
+
+    const fetchAllStudents = () =>
+        getAllStudents()
+            .then(res => res.json())
+            .then(data => setStudents(data));
+
+    useEffect(() => {
+        console.log("mounted");
+        fetchAllStudents();
+    }, []);
+
+    if (students.length <= 0) {
+        return "no data";
+    }
+
+    return students.map((student, index) => {
+        return <p key={index}> {student.id} {student.name} {student.email} {student.gender} </p>
+    });
 }
 
 export default App;
