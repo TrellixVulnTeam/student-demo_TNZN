39973887a8cc01ae4fe5c198db2a6a8a1916b552
Omar Shabaan <omar.plus.077@gmail.com>
2022-05-21 10:14:43 +0200

    add layout and table to view students data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,71 @@ import './App.css';
 
 import {getAllStudents} from "./client";
 import {useEffect, useState} from "react";
+import {
+    Layout,
+    Menu,
+    Breadcrumb,
+    Table
+} from 'antd';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+const { Header, Content, Footer, Sider } = Layout;
+
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
+
+
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',
+    }
+];
+
+
 
 function App() {
     const [students, setStudents] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
 
     const fetchAllStudents = () =>
         getAllStudents()
@@ -16,13 +78,70 @@ function App() {
         fetchAllStudents();
     }, []);
 
-    if (students.length <= 0) {
-        return "no data";
+    const renderStudents = () => {
+        if (students.length <= 0) {
+            return "no data available"
+        }
+
+        return <Table dataSource={students}
+                      columns={columns}/>;
+
+
     }
 
-    return students.map((student, index) => {
-        return <p key={index}> {student.id} {student.name} {student.email} {student.gender} </p>
-    });
+    // return students.map((student, index) => {
+    //     return <p key={index}> {student.id} {student.name} {student.email} {student.gender} </p>
+    // });
+    return (
+        <Layout
+            style={{
+                minHeight: '100vh',
+            }}
+        >
+            <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+                <div className="logo" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: '0 16px',
+                    }}
+                >
+                    <Breadcrumb
+                        style={{
+                            margin: '16px 0',
+                        }}
+                    >
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                        }}
+                    >
+                        {renderStudents()}
+                    </div>
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: 'center',
+                    }}
+                >
+                    Ant Design ©2018 Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
 }
 
 export default App;
