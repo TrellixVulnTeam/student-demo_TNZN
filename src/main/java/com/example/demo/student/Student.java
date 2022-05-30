f129750fb2617757205f0b393b16bfef24da9cf0
Omar Shabaan <omar.plus.077@gmail.com>
2022-05-30 14:18:14 +0200

    use javax.persistence annotation to create an db table from the Student class

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,12 +2,29 @@ package com.example.demo.student;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
 public class Student {
+  @SequenceGenerator(
+      name = "student_sequence",
+      sequenceName = "student_sequence",
+      allocationSize = 1
+  )
+  @GeneratedValue(
+      generator = "student_sequence",
+      strategy = GenerationType.SEQUENCE
+  )
+  @Id
   private Long id;
   private String name;
   private String email;
+  @Enumerated(EnumType.STRING)
   private Gender gender;
 }
